10899cd4442d23caa9339818ec33de7abe38ec8f
Mike Keen <mkeen.atl@gmail.com>
2011-06-02 18:26:45 -0400

    fixed function scope callback bug per http://k2xl.com/wordpress/2011/06/02/javascript-function-declaration-cautions/

--- js/kt_facebook.js
+++ js/kt_facebook.js
@@ -103,9 +103,7 @@ if(window.KT_API_SERVER && window.KT_API_KEY)
 	else if(params['method'] == 'apprequests'){
 	  //Stick uid, uuid, st1,st2,st3 in data
 	  params['data'] = KT_FB.kt.append_kt_tracking_info_to_apprequests(params['data'],uuid, st1, st2, st3);
-			console.log("declaring the apprequests function (good if AFTER feed declare)");
 		KT_CB_PATCHED = function(resp){
-			console.log("good one was called");
 	    if(resp){
 	      KT_FB.kt.kt_outbound_msg('ins',
 				       { u : uuid, s : uid,
@@ -117,14 +115,11 @@ if(window.KT_API_SERVER && window.KT_API_KEY)
 
 	  if(cb != undefined && cb!= null){
 	    var kt_cb = function(resp){
-				console.log("cb is not undefined");
 	      KT_CB_PATCHED(resp);
 	      cb(resp);
 	    };
 	  }else{
-			console.log("cb is undefined");
 	    var kt_cb = function(resp){
-				console.log("kt callback init");
 	      KT_CB_PATCHED(resp);
 	    };
 	  };
@@ -133,10 +128,8 @@ if(window.KT_API_SERVER && window.KT_API_KEY)
 	else if(params['method'] == 'feed'){
 	  if(params['link'] != undefined && params['link'] != null)
 	    params['link'] = KT_FB.kt.gen_stream_link(params['link'], uuid, st1, st2, st3);
-			console.log("declaring the feed function (not good)");
 
 	  KT_CB_PATCHED = function(resp){
-			console.log("bad one was called");
 	    if(resp){
 	      KT_FB.kt.kt_outbound_msg('pst',
 				       { tu : 'stream', u : uuid, s : uid,
